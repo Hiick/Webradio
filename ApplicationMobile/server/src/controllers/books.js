7e5800783960db7ee909627576db0ccc3f01ad86
Hiick <alexandre@cocoworker.com>
2020-01-24 16:17:22 +0100

    master

--- ApplicationMobile/server/src/controllers/books.js
+++ ApplicationMobile/server/src/controllers/books.js
@@ -7,7 +7,7 @@ const getBooks = (req, res) => {
             res.status(401).send(e)
         } else {
             res.status(200).json({
-                books: books,
+                book: books,
                 code: 200
             })
         }
