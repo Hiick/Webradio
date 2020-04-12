5f181b123cf0905c3a27aed09fbeb8d13ea018e4
Maysoon <maysoondoucanse@gmail.com>
2020-04-12 02:58:32 +0200

    recuperation des radios et affiche sur la page home

--- Site/Webradio/var/cache/dev/url_matching_routes.php
+++ Site/Webradio/var/cache/dev/url_matching_routes.php
@@ -13,28 +13,9 @@ return [
         '/_profiler/search_bar' => [[['_route' => '_profiler_search_bar', '_controller' => 'web_profiler.controller.profiler::searchBarAction'], null, null, null, false, false, null]],
         '/_profiler/phpinfo' => [[['_route' => '_profiler_phpinfo', '_controller' => 'web_profiler.controller.profiler::phpinfoAction'], null, null, null, false, false, null]],
         '/_profiler/open' => [[['_route' => '_profiler_open_file', '_controller' => 'web_profiler.controller.profiler::openAction'], null, null, null, false, false, null]],
-        '/adminuser' => [[['_route' => 'adminuser.users.index', '_controller' => 'App\\Controller\\Administrateur\\AdminUserController::index'], null, null, null, false, false, null]],
-        '/adminuser/channel' => [[['_route' => 'adminuser.channel.show', '_controller' => 'App\\Controller\\Administrateur\\AdminUserController::getchannel'], null, null, null, false, false, null]],
-        '/adminuser/channel/edit' => [[['_route' => 'adminuser.channel.edit', '_controller' => 'App\\Controller\\Administrateur\\AdminUserController::editUser'], null, null, null, false, false, null]],
-        '/admin/channel' => [[['_route' => 'admin.channel.index', '_controller' => 'App\\Controller\\Administrateur\\ChainesController::home'], null, null, null, false, false, null]],
-        '/admin/channel/userschannel' => [[['_route' => 'admin.channel.show', '_controller' => 'App\\Controller\\Administrateur\\ChainesController::allchannels'], null, null, null, false, false, null]],
-        '/admin/channel/edit' => [[['_route' => 'admin.channel.edit', '_controller' => 'App\\Controller\\Administrateur\\ChainesController::editChannel'], null, null, null, false, false, null]],
-        '/admin/channel/user' => [[['_route' => 'admin.channel.user', '_controller' => 'App\\Controller\\Administrateur\\ChainesController::getOneUser'], null, null, null, false, false, null]],
-        '/admin/adminuser' => [[['_route' => 'admin.adminuser.index', '_controller' => 'App\\Controller\\Administrateur\\CrudController::getUser'], null, null, null, false, false, null]],
-        '/admin/adminuser/create' => [[['_route' => 'admin.adminuser.create', '_controller' => 'App\\Controller\\Administrateur\\CrudController::addNewUser'], null, null, null, false, false, null]],
-        '/admin/user/edits' => [[['_route' => 'admin.adminuser.edit', '_controller' => 'App\\Controller\\Administrateur\\CrudController::editUser'], null, null, null, false, false, null]],
-        '/admin' => [[['_route' => 'admin.dashboard.index', '_controller' => 'App\\Controller\\Administrateur\\DashboardController::index'], null, null, null, false, false, null]],
-        '/admin/stats' => [[['_route' => 'admin.stats.show', '_controller' => 'App\\Controller\\Administrateur\\DashboardController::getStatistics'], null, null, null, false, false, null]],
-        '/admin/notification' => [[['_route' => 'admin.notifications.index', '_controller' => 'App\\Controller\\Administrateur\\NotificationsController::index'], null, null, null, false, false, null]],
-        '/admin/setting' => [[['_route' => 'admin.setting.index', '_controller' => 'App\\Controller\\Administrateur\\SettingsController::index'], null, null, null, false, false, null]],
-        '/admin/signalement' => [[['_route' => 'admin.signalement.index', '_controller' => 'App\\Controller\\Administrateur\\SignalementsController::index'], null, null, null, false, false, null]],
-        '/admin/signalement/users' => [[['_route' => 'admin.signalement.show', '_controller' => 'App\\Controller\\Administrateur\\SignalementsController::getUserSignal'], null, null, null, false, false, null]],
-        '/admin/signalement/listreporting' => [[['_route' => 'admin.signalement.listreporting', '_controller' => 'App\\Controller\\Administrateur\\SignalementsController::listReporting'], null, ['GET' => 0], null, false, false, null]],
-        '/admin/signalement/blankpage' => [[['_route' => 'admin.signalement.blankpage', '_controller' => 'App\\Controller\\Administrateur\\SignalementsController::blankPage'], null, ['GET' => 0], null, false, false, null]],
-        '/admin/user' => [[['_route' => 'admin.users.index', '_controller' => 'App\\Controller\\Administrateur\\UtilisateursController::index'], null, null, null, false, false, null]],
-        '/admin/users' => [[['_route' => 'admin.users.show', '_controller' => 'App\\Controller\\Administrateur\\UtilisateursController::listUsers'], null, null, null, false, false, null]],
-        '/' => [[['_route' => 'home.index', '_controller' => 'App\\Controller\\HomeController::home'], null, null, null, false, false, null]],
+        '/' => [[['_route' => 'home.index', '_controller' => 'App\\Controller\\HomeController::index'], null, null, null, false, false, null]],
         '/login' => [[['_route' => 'login', '_controller' => 'App\\Controller\\LoginController::login'], null, null, null, false, false, null]],
+        '/radio' => [[['_route' => 'radio.index', '_controller' => 'App\\Controller\\RadioController::index'], null, ['GET' => 0], null, true, false, null]],
         '/profile' => [[['_route' => 'profile.index', '_controller' => 'App\\Controller\\Users\\AdminUserController::index'], null, null, null, false, false, null]],
         '/profile/banni' => [[['_route' => 'profile.banni', '_controller' => 'App\\Controller\\Users\\AdminUserController::getUser'], null, null, null, false, false, null]],
         '/profile/page' => [[['_route' => 'profile.page', '_controller' => 'App\\Controller\\Users\\AdminUserController::messagePage'], null, null, null, false, false, null]],
@@ -44,9 +25,6 @@ return [
         '/profile/planning/delete' => [[['_route' => 'profile.planning.delete', '_controller' => 'App\\Controller\\Users\\PlanningController::delete'], null, null, null, false, false, null]],
         '/profile/planning/next' => [[['_route' => 'profile.planning.nextMonth', '_controller' => 'App\\Controller\\Users\\PlanningController::nextMonth'], null, null, null, false, false, null]],
         '/profile/planning/previousMonth' => [[['_route' => 'profile.planning.previousMonth', '_controller' => 'App\\Controller\\Users\\PlanningController::previousMonth'], null, null, null, false, false, null]],
-        '/profile/radio' => [[['_route' => 'profile.radios.index', '_controller' => 'App\\Controller\\Users\\RadiosController::index'], null, null, null, false, false, null]],
-        '/profile/radios' => [[['_route' => 'profile.radios.show', '_controller' => 'App\\Controller\\Users\\RadiosController::getAllRadio'], null, null, null, false, false, null]],
-        '/profile/banni/radios' => [[['_route' => 'profile.banni.radios', '_controller' => 'App\\Controller\\Users\\RadiosController::startHome'], null, null, null, false, false, null]],
         '/profile/setting' => [[['_route' => 'profile.setting.index', '_controller' => 'App\\Controller\\Users\\SettingsController::index'], null, null, null, false, false, null]],
         '/profile/setting/show' => [[['_route' => 'profile.setting.show', '_controller' => 'App\\Controller\\Users\\SettingsController::showUser'], null, null, null, false, false, null]],
         '/profile/timeline' => [[['_route' => 'profile.timeline.index', '_controller' => 'App\\Controller\\Users\\TimelineController::index'], null, null, null, false, false, null]],
@@ -69,14 +47,7 @@ return [
                         .'|(*:159)'
                     .')'
                 .')'
-                .'|/admin(?'
-                    .'|user/channel/([^/]++)(*:199)'
-                    .'|/(?'
-                        .'|channel/([^/]++)(*:227)'
-                        .'|notification/([^/]++)(*:256)'
-                        .'|setting/([^/]++)(*:280)'
-                    .')'
-                .')'
+                .'|/radio/([^/]++)(*:184)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
@@ -87,11 +58,8 @@ return [
         136 => [[['_route' => '_profiler_exception', '_controller' => 'web_profiler.controller.exception_panel::body'], ['token'], null, null, false, false, null]],
         149 => [[['_route' => '_profiler_exception_css', '_controller' => 'web_profiler.controller.exception_panel::stylesheet'], ['token'], null, null, false, false, null]],
         159 => [[['_route' => '_profiler', '_controller' => 'web_profiler.controller.profiler::panelAction'], ['token'], null, null, false, true, null]],
-        199 => [[['_route' => 'adminuser.channel.remove', '_controller' => 'App\\Controller\\Administrateur\\AdminUserController::removeUser'], ['id'], null, null, false, true, null]],
-        227 => [[['_route' => 'admin.channel.remove', '_controller' => 'App\\Controller\\Administrateur\\ChainesController::removeChannel'], ['id'], null, null, false, true, null]],
-        256 => [[['_route' => 'admin.notifications.sendnotif', '_controller' => 'App\\Controller\\Administrateur\\NotificationsController::sendNotif'], ['id'], null, null, false, true, null]],
-        280 => [
-            [['_route' => 'admin.setting.edit', '_controller' => 'App\\Controller\\Administrateur\\SettingsController::editUser'], ['id'], ['GET' => 0, 'POST' => 1], null, false, true, null],
+        184 => [
+            [['_route' => 'radio.show', '_controller' => 'App\\Controller\\RadioController::show'], ['id'], ['GET' => 0], null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
