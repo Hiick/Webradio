7c3bfdd2c44200222f7e4777d5a5fde6a541ab13
EmericBayard <57359277+EmericBayard@users.noreply.github.com>
2020-04-17 11:31:38 +0200

    tentative de push

---
+++ src/app/app.module.ts
@@ -0,0 +1,103 @@
+import { Login_1Page } from './../pages/login-1/login-1';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+//http request
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
+//FB
+import { SocialLoginModule, AuthServiceConfig, FacebookLoginProvider } from 'angularx-social-login';
+//ENDFB
+
+
+
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { RegisterPage } from '../pages/register/register';
+import { LoginPage } from '../pages/login/login';
+import { RadioSubscribePage } from '../pages/radio-subscribe/radio-subscribe';
+import { AccountPage } from '../pages/account/account';
+import { HomepageRadioPage } from '../pages/homepage-radio/homepage-radio';
+import { HomepageChainePage } from '../pages/homepage-chaine/homepage-chaine';
+import { Chaine_9Page } from '../pages/chaine-9/chaine-9';
+import { ChaineRecPage } from '../pages/chaine-rec/chaine-rec';
+
+
+//FB
+const config = new AuthServiceConfig([
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('2203659926599837')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
+//ENDFB
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    RegisterPage,
+    LoginPage,
+    Login_1Page,
+    RadioSubscribePage,
+    AccountPage,
+    HomepageRadioPage,
+    HomepageChainePage,
+    Chaine_9Page,
+    ChaineRecPage
+  ],
+  imports: [
+    BrowserModule,
+    //FB
+    SocialLoginModule,
+    //ENDFB
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
+    HttpModule,
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    RegisterPage,
+    LoginPage,
+    Login_1Page,
+    RadioSubscribePage,
+    AccountPage,
+    HomepageRadioPage,
+    HomepageChainePage,
+    Chaine_9Page,
+    ChaineRecPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler, },
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+
+  ]
+})
+export class AppModule {}
