396dca8a1bf25338fe66c71cdabbcfdf9d21c95f
Hiick <alexandre@cocoworker.com>
2020-04-21 15:38:30 +0200

    New: Isolate API

--- ApplicationMobile/server/src/routes/open/authRoutes.js
+++
@@ -1,9 +0,0 @@
-module.exports =  (router, app, authRoutesMethods) => {
-
-    router.post('/register', authRoutesMethods.register);
-    router.post('/login', app.oauth.grant(), authRoutesMethods.login);
-    router.get('/facebook/login/:token', authRoutesMethods.facebookLogin);
-
-    return router
-
-};
